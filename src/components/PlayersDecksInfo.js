3ac3055dd9b0434d495e6d54833f4f08b4a5ae64
PG <plamen5020@gmail.com>
2023-10-18 21:53:59 +0300

    added show players info button

--- src/components/PlayersDecksInfo.js
+++ src/components/PlayersDecksInfo.js
@@ -1,43 +1,60 @@
-import { collection, doc, getDocs } from "firebase/firestore"
+import { collection, getDocs } from "firebase/firestore"
 import { db } from "../config/firebase"
 import { useState, useEffect } from "react";
 
 
-function PlayersDecksInfo () {
+function PlayersDecksInfo () {    
 
     const [players, setPlayers] = useState([]);
+    const [showPlayerInfo, setShowPlayerInfo] = useState(false);
 
     const playerInfo = async () => {
-        const playersRef = collection(db, "players");
+        const playersRef = collection(db, 'players');
         const querySnapshot = await getDocs(playersRef);
-        
-        const playerData = [];
 
+        const playerData = [];
         querySnapshot.forEach((doc) => {
             const data = doc.data();
             playerData.push(data);
         });
 
-        console.log(playerData);
         setPlayers(playerData);
-    }
+        setShowPlayerInfo(true);        
+    };
 
     useEffect(() => {
         // Automatically fetch player data when the component mounts
-        playerInfo();
+        setShowPlayerInfo(false);
     }, []);
+
+
 
     return (
         <div>
             <h3>Players:</h3>
             <button onClick={playerInfo}>Load Player Info</button>
-            <ul>
+
+            <div className="playerInfo">
+            {showPlayerInfo && (
+            <div>
                 {players.map((player, index) => (
-                <li key={index}>
-                    {player.player} - Decks: {JSON.stringify(player.decks)}
-                </li>
-                ))}
-            </ul>
+                <div key={index} className="player-info">
+                    <h4>Player: {player.player}</h4>
+
+                    <div>
+                        Decks: 
+                        {Object.keys(player.decks).map((deckName) => (
+                            <div key={deckName}>
+                            {deckName} - {player.decks[deckName]}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            ))}
+            </div>
+        )}
+            </div>
+            
         </div>
     )
 }
